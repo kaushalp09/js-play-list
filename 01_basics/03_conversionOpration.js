ae2f953d0e6caafc46ee8378be8ee7a43619e393
kaushalp09 <169058230+kaushalp09@users.noreply.github.com>
2024-05-29 08:01:14 +0000

    comperision

--- 01_basics/03_conversionOpration.js
+++ 01_basics/03_conversionOpration.js
@@ -15,7 +15,7 @@ let valueInNUmber = Number(score)
 let isLoggedIn = "kauhsal"
 
 let booleanISLoggedIn = Boolean(isLoggedIn)
-console.log(booleanISLoggedIn);
+// console.log(booleanISLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -24,4 +24,43 @@ console.log(booleanISLoggedIn);
 let someNUmber = "33"
 
 let stringNumber = String(someNUmber)
-console.log(someNUmber)
+// console.log(someNUmber)
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "kaushal"
+
+let str3= str1 + str2
+// console.log(str3)
+
+// console.log(2+2);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num5 , num2 ,num3,
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter =100
+gameCounter++;
+
+console.log(gameCounter)

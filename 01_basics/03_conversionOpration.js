67c9446456fce9921092e8f4c1fba09010a0f57b
kaushalp09 <169058230+kaushalp09@users.noreply.github.com>
2024-05-29 07:26:20 +0000

    Conversion of dtatype

---
+++ 01_basics/03_conversionOpration.js
@@ -0,0 +1,27 @@
+let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+let valueInNUmber = Number(score)
+// console.log(typeof valueInNUmber);
+// console.log( (valueInNUmber));
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "kauhsal"
+
+let booleanISLoggedIn = Boolean(isLoggedIn)
+console.log(booleanISLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNUmber = 33
+
+let stringNumber = String(someNUmber)
+console.log(someNUmber)
